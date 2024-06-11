7b13506401101d1fb62d3310d347e8c11c9e6822
Alejandro Ruiz Crespo <113971371+a22rucral@users.noreply.github.com>
2024-06-11 15:57:01 +0200

    Update Gato.py

--- Gato.py
+++ Gato.py
@@ -3,18 +3,11 @@ Clase Gato.
 :author: Jaime Rabasco Ronda.
 """
 class Gato:
-
-    def __init__(self):
-        """
-        Inicializa un objeto de la clase gato
-        """
-        self.maulla = 'Miau'
-
     def maullar(self):
         """
         FUnción que imprime el maullar de un gato
         """
-        print(self.maulla)
+        print('Miau')
 
 g = Gato()
 g.maullar()
